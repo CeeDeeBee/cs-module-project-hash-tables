a588a9a92820bf33045a9b05fbf985f9a93c0155
CeeDeeBee <cdbarnes98@gmail.com>
2020-08-06 00:24:06 -0400

    Add lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,8 @@
+import math
+import random
+from hashtable import HashTable
 # Your code here
+cache = HashTable(8)
 
 
 def slowfun_too_slow(x, y):
@@ -9,17 +13,24 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    key = str(x) + str(y)
+    value = cache.get(key)
+    if value:
+        return value
 
+    value = slowfun_too_slow(x, y)
+    cache.put(key, value)
+
+    return value
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
