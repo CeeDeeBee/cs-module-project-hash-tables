02fb81e2370c912fa58b15eb529f1fd7af399bae
CeeDeeBee <cdbarnes98@gmail.com>
2020-08-06 17:28:37 -0400

    Add crack caesar

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,29 @@
 # decode it.
 
 # Your code here
+standard_frequencies = ["E", "T", "A", "O", "H", "N", "R", "I", "S", "D", "L",
+                        "W", "U", "G", "F", "B", "M", "Y", "C", "P", "K", "V", "Q", "J", "X", "Z"]
 
+with open("ciphertext.txt") as f:
+    words = f.read()
+
+letter_count = {}
+for char in words:
+    if char.isalpha():
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+
+sorted_count = {k: v for k, v in sorted(
+    letter_count.items(), key=lambda item: item[1], reverse=True)}
+
+cypher_map = {}
+for i, key in enumerate(sorted_count.keys()):
+    cypher_map[key] = standard_frequencies[i]
+
+for char in words:
+    if char.isalpha():
+        print(cypher_map[char], end="")
+    else:
+        print(char, end="")
