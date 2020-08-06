406ee73fe472a4021271c1b099153e3ae04f9aa7
CeeDeeBee <cdbarnes98@gmail.com>
2020-08-06 00:51:01 -0400

    Add expensive seq

---
+++ applications/expensive_seq/hashtable.py
@@ -0,0 +1,210 @@
+class HashTableEntry:
+    """
+    Linked List hash table key/value pair
+    """
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+# Hash table can't have fewer than this many slots
+MIN_CAPACITY = 8
+
+
+class HashTable:
+    """
+    A hash table that with `capacity` buckets
+    that accepts string keys
+
+    Implement this.
+    """
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = [None] * capacity
+        self.size = 0
+
+    def get_num_slots(self):
+        """
+        Return the length of the list you're using to hold the hash
+        table data. (Not the number of items stored in the hash table,
+        but the number of slots in the main list.)
+
+        One of the tests relies on this.
+
+        Implement this.
+        """
+        return self.capacity
+
+    def get_load_factor(self):
+        """
+        Return the load factor for this hash table.
+
+        Implement this.
+        """
+        return self.size / self.capacity
+
+    def fnv1(self, key):
+        """
+        FNV-1 Hash, 64-bit
+
+        Implement this, and/or DJB2.
+        """
+        FNV_prime = 1099511628211
+        offset_basis = 14695981039346656037
+
+        hashed_key = offset_basis
+
+        byte_string = key.encode()
+        for b in byte_string:
+            hashed_key *= FNV_prime
+            hashed_key ^= b
+
+        return hashed_key
+
+    def djb2(self, key):
+        """
+        DJB2 hash, 32-bit
+
+        Implement this, and/or FNV-1.
+        """
+        # Your code here
+
+    def hash_index(self, key):
+        """
+        Take an arbitrary key and return a valid integer index
+        between within the storage capacity of the hash table.
+        """
+        return self.fnv1(key) % self.capacity
+        # return self.djb2(key) % self.capacity
+
+    def put(self, key, value):
+        """
+        Store the value with the given key.
+
+        Hash collisions should be handled with Linked List Chaining.
+
+        Implement this.
+        """
+        store_index = self.hash_index(key)
+        if not self.storage[store_index]:
+            self.storage[store_index] = HashTableEntry(key, value)
+        else:
+            node = self.storage[store_index]
+            if node.key == key:
+                node.value = value
+                return
+            while node.next:
+                node = node.next
+                if node.key == key:
+                    node.value = value
+                    return
+            node.next = HashTableEntry(key, value)
+            self.size += 1
+            if self.get_load_factor() > 0.7:
+                self.resize(self.capacity * 2)
+
+    def delete(self, key):
+        """
+        Remove the value stored with the given key.
+
+        Print a warning if the key is not found.
+
+        Implement this.
+        """
+        store_index = self.hash_index(key)
+        if not self.storage[store_index]:
+            print(f"Error: no value stored at {key}")
+        else:
+            prev = None
+            node = self.storage[store_index]
+            if node.key == key:
+                self.storage[store_index] = node.next
+                self.dec_size()
+                return
+            while node.next:
+                prev = node
+                node = node.next
+                if node.key == key:
+                    prev.next = node.next
+                    self.dec_size()
+                    return
+
+            print(f"Error: no value stored at {key}")
+
+    def get(self, key):
+        """
+        Retrieve the value stored with the given key.
+
+        Returns None if the key is not found.
+
+        Implement this.
+        """
+        store_index = self.hash_index(key)
+        node = self.storage[store_index]
+        while node:
+            if node.key == key:
+                return node.value
+            node = node.next
+
+        return None
+
+    def resize(self, new_capacity):
+        """
+        Changes the capacity of the hash table and
+        rehashes all key/value pairs.
+
+        Implement this.
+        """
+        old_storage = self.storage
+        self.storage = [None] * new_capacity
+        self.capacity = new_capacity
+        for i in old_storage:
+            node = i
+            while node:
+                self.put(node.key, node.value)
+                node = node.next
+
+    def dec_size(self):
+        self.size -= 1
+        if self.get_load_factor() < 0.2 and self.capacity > 8:
+            new_size = max(self.capacity / 2, 8)
+            self.resize(new_size)
+
+
+if __name__ == "__main__":
+    ht = HashTable(8)
+
+    ht.put("line_1", "'Twas brillig, and the slithy toves")
+    ht.put("line_2", "Did gyre and gimble in the wabe:")
+    ht.put("line_3", "All mimsy were the borogoves,")
+    ht.put("line_4", "And the mome raths outgrabe.")
+    ht.put("line_5", '"Beware the Jabberwock, my son!')
+    ht.put("line_6", "The jaws that bite, the claws that catch!")
+    ht.put("line_7", "Beware the Jubjub bird, and shun")
+    ht.put("line_8", 'The frumious Bandersnatch!"')
+    ht.put("line_9", "He took his vorpal sword in hand;")
+    ht.put("line_10", "Long time the manxome foe he sought--")
+    ht.put("line_11", "So rested he by the Tumtum tree")
+    ht.put("line_12", "And stood awhile in thought.")
+
+    print("")
+
+    # Test storing beyond capacity
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
+
+    # Test resizing
+    old_capacity = ht.get_num_slots()
+    ht.resize(ht.capacity * 2)
+    new_capacity = ht.get_num_slots()
+
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+
+    # Test if data intact after resizing
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
+
+    print("")
